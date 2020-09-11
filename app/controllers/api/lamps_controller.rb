6e1f54d1b1b6ed39088a4f4668a8c7aa3a42cf04
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-11 12:42:22 -0500

    adding show action for lamps

--- app/controllers/api/lamps_controller.rb
+++ app/controllers/api/lamps_controller.rb
@@ -3,4 +3,9 @@ class Api::LampsController < ApplicationController
     @lamps = Lamp.all
     render 'index.json.jb'
   end
+
+  def show
+    @lamp = Lamp.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/lamps" => "lamps#index"
+    get "/lamps/:id" => "lamps#show"
+
   end
 end
